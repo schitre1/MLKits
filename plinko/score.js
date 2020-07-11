ae2bd1fc8c2dceb8f3beea9e6bfbfe156e06024b
Chitre <schitre@paypal.com>
2020-07-10 23:39:11 -0700

    vary the value of k over a range

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,5 @@
 const outputs = [];
 const predictionPoint = 300;
-let k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -13,25 +12,21 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 function runAnalysis() {
   // Write code here to analyze stuff
   //Call split data set
-  const testSetSize = 10;
+  const testSetSize = 100;
   const [testSet, trainingSet] = splitDataSet(outputs, testSetSize); //test data size fixed at 10
 
-  // old way of doing things:
-  // let numCorrect = 0;
-  // for (let i = 0; i < testSet.length; i++) {
-  //   const bucket = knn(trainingSet, testSet[i][0]);
-  //   if (bucket === testSet[i][3]) numCorrect++;
-  // }
+  _.range(1, 20).forEach(k => {
+    // use lodash for same calc
+    //we need to compare how accurate the prediction was
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
 
-  // use lodash for same calc
-  //we need to compare how accurate the prediction was
-  const accuracy = _.chain(testSet)
-    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
-    .size()
-    .divide(testSetSize)
-    .value();
+    console.log('Accuracy with k ', k, accuracy);
+  });
 
-  console.log('Accuracy', accuracy);
 
 
 }
@@ -51,7 +46,7 @@ function splitDataSet(data, testCount) { //testCount number of records in test s
 
 //for training we will use trainingSet
 //point is point from where ball is dropped, for which we are trying to make a prediction
-function knn(data, point) {
+function knn(data, point, k) {
   return _.chain(data)
     .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
